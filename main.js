6fcaae967203725167868fd23a2252c512ec7ffe
Tinctoria <quizzie_89@hotmail.com>
2021-06-04 23:11:27 +0200

    lyric field empties between searches.

--- main.js
+++ main.js
@@ -14,15 +14,19 @@ function myErrorMsg(text) {
 
 // Funktion som kollar om inputfälten är tomma.
 function checkInputFields(event) {
+    lyrics.querySelectorAll('p').forEach(n => n.remove());
+    lyrics.querySelectorAll('textarea').forEach(n => n.remove());
+    lyrics.querySelectorAll('h2').forEach(n => n.remove());
+
     if(myArtist.value==="" || myTitle.value==="") {
         event.preventDefault();
-        console.log("No");
+        console.log("No, no button click for you.");
         myErrorMsg("You have to enter both an artist and a song title for this to work.");
     }
     else {
         event.preventDefault();
-
-        let URL="https://api.lyrics.ovh/v1/" + encodeURI(myArtist.value) +"/" + encodeURI(myTitle.value);  
+        console.log("Ok, you can click.");
+                let URL="https://api.lyrics.ovh/v1/" + encodeURI(myArtist.value) +"/" + encodeURI(myTitle.value);  
 
         fetch(URL).then(function(response){
             response.json().then(function(data){
@@ -30,18 +34,22 @@ function checkInputFields(event) {
             myLyrics.textContent=data.lyrics;
         
         //OM FETCH LYCKAS
-
+           
             const mySearch=document.createElement("h2"); 
             mySearch.textContent="Lyrics for " + myTitle.value+ " by " + myArtist.value;
+            
+            
             lyrics.appendChild(mySearch);
             lyrics.appendChild(myLyrics);
             myArtist.value="";
             myTitle.value="";
+            
 
         });
 
         }).catch(function(){
         //OM FETCH MISSLYCKAS
+        //Denna tar lång tid att ladda, kan man korta ner detta på något vis eller är det APIn som gör det? 
             myErrorMsg("Sorry, we could not find that song.")
     });
     }
