7f22dd335e6a98c112e8a40e6a752e7a55e77fd5
Tinctoria <quizzie_89@hotmail.com>
2021-06-04 09:26:00 +0200

    added function for errormessage and a check for content in input fields

--- main.js
+++ main.js
@@ -2,32 +2,50 @@ const form=document.getElementById("form");
 const searchBtn=form.querySelector("button");
 const lyrics=document.getElementById("lyrics-here");
 const errorMsg=document.getElementById("error"); 
+const myArtist=document.getElementById("artist"); 
+const myTitle=document.getElementById("title"); 
 
-searchBtn.addEventListener("click", function(event){
-    event.preventDefault();
+//Funktion för felmeddelanden.
+function myErrorMsg(text) {
+        const myError=document.createElement("p");
+        myError.textContent=text;
+        errorMsg.appendChild(myError);
+}
 
-    const myArtist=document.getElementById("artist"); 
-    const myTitle=document.getElementById("title"); 
+// Funktion som kollar om inputfälten är tomma.
+function checkInputFields(event) {
+    if(myArtist.value==="" || myTitle.value==="") {
+        event.preventDefault();
+        console.log("No");
+        myErrorMsg("You have to enter both an artist and a song title for this to work.");
+    }
+    else {
+        event.preventDefault();
 
-    let URL="https://api.lyrics.ovh/v1/" + encodeURI(myArtist.value) +"/" + encodeURI(myTitle.value);
-    console.log(myArtist.value); 
-    console.log(myTitle.value);
+        let URL="https://api.lyrics.ovh/v1/" + encodeURI(myArtist.value) +"/" + encodeURI(myTitle.value);  
 
-    fetch(URL).then(function(response){
-        response.json().then(function(data){
-        const myLyrics=document.createElement("p"); 
-        myLyrics.textContent=data.lyrics;
-        //IF FETCH LYCKAS
+        fetch(URL).then(function(response){
+            response.json().then(function(data){
+            const myLyrics=document.createElement("textarea"); 
+            myLyrics.textContent=data.lyrics;
         
-        lyrics.appendChild(myLyrics);
+        //OM FETCH LYCKAS
+
+            const mySearch=document.createElement("h2"); 
+            mySearch.textContent="Lyrics for " + myTitle.value+ " by " + myArtist.value;
+            lyrics.appendChild(mySearch);
+            lyrics.appendChild(myLyrics);
+            myArtist.value="";
+            myTitle.value="";
 
         });
-    }).catch(function(){
-        //IF FETCH MISSLYCKAS
-        
-        const myError=document.createElement("p");
-        myError.textContent="Sorry, we cannot find that song.";
-        errorMsg.appendChild(myError);
+
+        }).catch(function(){
+        //OM FETCH MISSLYCKAS
+            myErrorMsg("Sorry, we could not find that song.")
     });
-    
-});
+    }
+}
+
+
+searchBtn.addEventListener("click", checkInputFields);
